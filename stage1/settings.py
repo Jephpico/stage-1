de241aecf9ee9dc49b7b592600f978d575073521
jephpico <amadasun.jephthah@gmail.com>
2022-11-01 12:59:45 +0100

    heroku

--- stage1/settings.py
+++ stage1/settings.py
@@ -125,3 +125,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+import django_on_heroku
+import dj_database_url
+django_on_heroku.settings(locals())
