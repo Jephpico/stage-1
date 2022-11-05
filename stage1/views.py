248f3c9797653d7124e128f53c22ce3071d921ea
jephpico <amadasun.jephthah@gmail.com>
2022-11-06 00:32:04 +0100

    na wa

--- stage1/views.py
+++ stage1/views.py
@@ -11,41 +11,38 @@ class SlackdeetView(APIView):
     slackdeets = {"slackUsername": "Lee", "backend": True, "age": 27, "bio":"my name is lee, and i have just one goal here; becoming a finalist!"}
 
     def get(self, request):
-        slackdeets = {"slackUsername": "Lee", "backend": True, "age": 27, "bio":"my name is lee, and i have just one goal here; becoming a finalist!"}
+        slackdeets = {"slackUsername": "Lee", "backend": True, "age": 29, "bio":"my name is lee, and i have just one goal here; becoming a finalist!"}
         #json_slackdeet = json.dumps(slackdeets, safe=False)
-        return JsonResponse(slackdeets)
+        return Response(slackdeets)
     def post(self, request):
-
+    
             data = request.data
 
-            operator_type = data['operator_type']
-            integer1 = data["x"]
-            integer2 = data["y"]
-            if operator_type == ('addition', 'add', 'plus', '+', 'sum'):
-                result = integer1 + integer2
-                deet = {}
-                deet['slackUsername'] = 'Lee'
-                deet['result']= result
-                deet['operation_type'] = operator_type
-                return Response(deet)
+            op_type = data['operator_type']
+            integer1 = int(data['x'])
+            integer2 = int(data['y'])
 
+            if op_type in ['addition', 'add', 'plus', '+', 'sum']:
+                result = integer1+integer2
 
-            elif operator_type == ('subtraction', 'minus','-' ):
-                result = integer1 - integer2
-                deet = {}
-                deet['slackUsername'] = 'Lee'
-                deet['result']= result
-                deet['operation_type'] = operator_type
-                return Response(deet)
-
+            elif op_type in ['subtraction', 'minus','-' ]:
+                result = integer1-integer2
+            
+            elif op_type in ['multiplication', 'times', 'multiply', '*' ]:
+                result = integer1*integer2
+             
             else:
-                operator_type == ('multiplication', 'times', 'multiply', '*' , 'x', 'X')
-                result = integer1 * integer2
-                deet = {}
-                deet['slackUsername'] = 'Lee'
-                deet['result']= result
-                deet['operation_type'] = operator_type
-                return Response(deet)
+                 op_type = "invalid operation type"
+                
+
+            computed_data = {
+                "slackUsername":"lee",
+                "result": result,
+                "operator_type": op_type
+            }
+            return Response(computed_data, status=status.HTTP_200_OK)
+
+
 
         
         
